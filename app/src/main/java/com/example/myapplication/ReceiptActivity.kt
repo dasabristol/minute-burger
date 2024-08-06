299657a09fee88eb7375fee2b3e9bdc8be9ecdc6
dasabristol <163862523+dasabristol@users.noreply.github.com>
2024-08-06 12:52:24 +0800

    HADUEKEN

--- app/src/main/java/com/example/myapplication/ReceiptActivity.kt
+++ app/src/main/java/com/example/myapplication/ReceiptActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 
 class ReceiptActivity : AppCompatActivity() {
@@ -13,6 +15,14 @@ class ReceiptActivity : AppCompatActivity() {
         val receiptTextView: TextView = findViewById(R.id.receiptTextView)
         val receiptData = intent.getStringExtra("RECEIPT_DATA")
 
+        val btnOrderAgain = findViewById<Button>(R.id.OrderAgain)
+
         receiptTextView.text = receiptData
+
+        btnOrderAgain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
